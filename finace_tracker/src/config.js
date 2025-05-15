18ae7ed9f1d5de25995ab254dd2859a28baa83e2
agrawalpri <pka26082002@gmail.com>
2025-05-15 18:45:57 +0530

    Initialize project structure with components and pages folders

---
+++ finace_tracker/src/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBinOTtKr_qr0KSk_YT4PRHbiblOz6pEVs",
+  authDomain: "finance-tracker-98192.firebaseapp.com",
+  projectId: "finance-tracker-98192",
+  storageBucket: "finance-tracker-98192.firebasestorage.app",
+  messagingSenderId: "432079124063",
+  appId: "1:432079124063:web:0451b8a735d347eca90dea",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
